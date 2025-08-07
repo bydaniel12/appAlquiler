14a39e1cbaa794e52630571df42a9d62610f58b2
bydaniel12 <djuchiha12@gmail.com>
2025-08-06 22:50:17 -0500

    Mejora en formulario de boleta

--- src/pages/admin/PaymentForm.jsx
+++ src/pages/admin/PaymentForm.jsx
@@ -251,9 +251,6 @@ export const PaymentForm = ({
               <span className="absolute inset-y-0 start-3 -translate-y-4.5 bg-white px-0.5 text-sm font-medium text-gray-700 transition-transform peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-4.5">
                 Internet
               </span>
-              <span className="absolute inset-y-0 start-2 bg-white px-0.5 text-sm font-medium text-gray-700 transition-transform peer-placeholder-shown:translate-y-0 peer-focus:-translate-y-4.5">
-                S/
-              </span>
             </label>
           </div>
 
